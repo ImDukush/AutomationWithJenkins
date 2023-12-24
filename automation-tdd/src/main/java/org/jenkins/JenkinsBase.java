0778e70eab03f1a5256ce936c7c7c7a4ead28877
Durgesh Kumar Sharma <imdukush@yahoo.com>
2023-12-24 13:15:38 +0530

    JenkinsBase.java added to framework

---
+++ automation-tdd/src/main/java/org/jenkins/JenkinsBase.java
@@ -0,0 +1,8 @@
+package org.jenkins;
+
+public class JenkinsBase {
+
+    public static void main(String[] args) {
+        System.out.println("I have started jenkins and executed it successfully");
+    }
+}
